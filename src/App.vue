cfbff539df1d44b70f84438cc47da12fc77eb45b
marielmedina <nenems08hdz@gmail.com>
2026-05-19 14:01:42 -0600

    Ajustes en el diseño de la interfaz

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
-<script setup></script>
+<script setup>
+import { RouterView } from 'vue-router'
+import sidebar from './components/sidebar.vue'
+import navbar from './components/navbar.vue' 
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-</template>
+  <div class="flex min-h-screen bg-gray-50 text-gray-800 font-sans antialiased">
+    
+    <sidebar />
 
-<style scoped></style>
+    <div class="flex-1 flex flex-col">
+      
+      <navbar />
+
+      <main class="flex-1 p-8 overflow-y-auto">
+        <RouterView />
+      </main>
+
+    </div>
+
+  </div>
+</template>
---
+++ src/components/navbar.vue
@@ -0,0 +1,47 @@
+<script setup>
+// lógica de JavaScript más adelante
+// Variables para saber qué area se eligió, funciones de clics, etc.)
+</script>
+
+<template>
+  <header class="h-16 bg-white border-b border border-gray-200 px-8 flex items-center justify-between shadow-sm">
+    
+    <div class="flex items-center gap-6">
+
+      <div class="flex flex-col gap-1">
+        <label class="text-xs font-semibold text-gray-500 uppercase tracking-wider">
+          area
+        </label>
+        <select class="bg-white border border-gray-300 rounded-md py-1 px-3 text-sm text-gray-700 focus:outline-none">
+          <option>Todas las Areas</option>
+          <option>Planta Norte</option>
+          <option>Planta Sur</option>
+        </select>
+      </div>
+
+      <div class="flex flex-col gap-1">
+        <label class="text-xs font-semibold text-gray-500 uppercase tracking-wider">
+          departamento
+        </label>
+        <select class="bg-white border border-gray-300 rounded-md py-1 px-3 text-sm text-gray-700 focus:outline-none">
+          <option>Todas los departamentos</option>
+          <option>Producción</option>
+          <option>Calidad</option>
+          <option>Mantenimiento</option>
+        </select>
+        
+      </div>
+
+    </div>
+
+    <div class="flex items-center gap-5">
+      <button class="bg-[#3f2a52] hover:bg-[#beaed8] text-white text-sm font-medium py-2 px-4 rounded-md focus:outline-none">
+       Personalizar Dashboard
+      </button>
+      <span class="text-gray-400 text-sm">perfil</span>
+     
+    </div>
+
+  </header>
+</template>
+
